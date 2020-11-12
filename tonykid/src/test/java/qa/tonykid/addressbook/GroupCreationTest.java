d74d3c8e23ae8a579f34d528cefb96ba2461c355
TonyKid89 <antonpavlyk89@gmail.com>
2020-11-12 16:04:58 +0200

    Added init to BeforeClass

--- tonykid/src/test/java/qa/tonykid/addressbook/GroupCreationTest.java
+++ tonykid/src/test/java/qa/tonykid/addressbook/GroupCreationTest.java
@@ -8,44 +8,50 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
 import java.util.concurrent.TimeUnit;
 
 public class GroupCreationTest {
-  private WebDriver driver;
+    private WebDriver driver;
 
-  @BeforeMethod
-  public void setUp() {
-    driver = new ChromeDriver();
-    driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+    @BeforeMethod
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("http://localhost/addressbook/group.php");
+        driver.findElement(By.id("LoginForm")).click();
+        driver.findElement(By.name("user")).click();
+        driver.findElement(By.name("user")).clear();
+        driver.findElement(By.name("user")).sendKeys("admin");
+        driver.findElement(By.name("pass")).click();
+        driver.findElement(By.name("pass")).clear();
+        driver.findElement(By.name("pass")).sendKeys("secret");
+        driver.findElement(By.cssSelector("input:nth-child(7)")).click();
+    }
 
-  }
-  @AfterMethod
-  public void tearDown() {
-    driver.quit();
-  }
-  @Test
-  public void qt() {
-    driver.get("http://localhost/addressbook/group.php");
 
-    driver.manage().window().maximize();
-    driver.findElement(By.id("LoginForm")).click();
-    driver.findElement(By.name("user")).click();
-    driver.findElement(By.name("user")).clear();
-    driver.findElement(By.name("user")).sendKeys("admin");
-    driver.findElement(By.name("pass")).click();
-    driver.findElement(By.name("pass")).clear();
-    driver.findElement(By.name("pass")).sendKeys("secret");
-    driver.findElement(By.cssSelector("input:nth-child(7)")).click();
-    driver.findElement(By.linkText("groups")).click();
-    driver.findElement(By.name("new")).click();
-    driver.findElement(By.name("group_name")).click();
-    driver.findElement(By.name("group_name")).sendKeys("test1");
-    driver.findElement(By.name("group_header")).click();
-    driver.findElement(By.name("group_header")).sendKeys("test2");
-    driver.findElement(By.name("group_footer")).click();
-    driver.findElement(By.name("group_footer")).sendKeys("test3");
-    driver.findElement(By.cssSelector("form:nth-child(2)")).click();
-    driver.findElement(By.name("submit")).click();
-    driver.findElement(By.linkText("group page")).click();
-  }
+    @Test
+    public void testGroupCreation() {
+
+        driver.findElement(By.linkText("groups")).click();
+        driver.findElement(By.name("new")).click();
+        driver.findElement(By.name("group_name")).click();
+        driver.findElement(By.name("group_name")).clear();
+        driver.findElement(By.name("group_name")).sendKeys("test1");
+        driver.findElement(By.name("group_header")).click();
+        driver.findElement(By.name("group_header")).clear();
+        driver.findElement(By.name("group_header")).sendKeys("test2");
+        driver.findElement(By.name("group_footer")).click();
+        driver.findElement(By.name("group_footer")).clear();
+        driver.findElement(By.name("group_footer")).sendKeys("test3");
+        driver.findElement(By.cssSelector("form:nth-child(2)")).click();
+        driver.findElement(By.name("submit")).click();
+        driver.findElement(By.linkText("group page")).click();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
 }
